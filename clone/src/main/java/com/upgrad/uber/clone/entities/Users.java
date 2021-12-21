9a767a0c8a3e42f524ba2480591bfa43b5d71bb1
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-21 15:03:09 +0530

    Users Entity

--- clone/src/main/java/com/upgrad/uber/clone/entities/Users.java
+++ clone/src/main/java/com/upgrad/uber/clone/entities/Users.java
@@ -1,4 +1,145 @@
 package com.upgrad.uber.clone.entities;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonManagedReference;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "users")
 public class Users {
+    @Id
+    @Column(name="user_id")
+    @GeneratedValue
+    private int userId;
+
+    @Column(name="first_name",nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(name="mobile_no",nullable = false,unique = true,length = 10)
+    private String mobileNo;
+
+    @Column(name="wallet_money")
+    private double walletMoney=10000.00 ;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "user",fetch = FetchType.LAZY,cascade = {CascadeType.ALL})
+    private Set<Booking> bookings;
+
+    @JsonManagedReference
+    @ManyToOne
+    @JoinColumn(name = "role_id",nullable = false)
+    private Role role;
+
+    public Users() {
+
+    }
+
+    @Override
+    public String toString() {
+        return "Users{" +
+                "userId=" + userId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", mobileNo='" + mobileNo + '\'' +
+                ", walletMoney=" + walletMoney +
+                ", role=" + role +
+                '}';
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMobileNo() {
+        return mobileNo;
+    }
+
+    public void setMobileNo(String mobileNo) {
+        this.mobileNo = mobileNo;
+    }
+
+    public double getWalletMoney() {
+        return walletMoney;
+    }
+
+    public void setWalletMoney(double walletMoney) {
+        this.walletMoney = walletMoney;
+    }
+
+    public Set<Booking> getBookings() {
+        return bookings;
+    }
+
+    public void setBookings(Set<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public Role getRole() {
+        return role;
+    }
+
+    public void setRole(Role role) {
+        this.role = role;
+    }
+
+    public Users( String firstName, String lastName, String password, String email, String mobileNo, double walletMoney, Role role) {
+
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.password = password;
+        this.email = email;
+        this.mobileNo = mobileNo;
+        this.walletMoney = walletMoney;
+        this.role = role;
+    }
+
 }
