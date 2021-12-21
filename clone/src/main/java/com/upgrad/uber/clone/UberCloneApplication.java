85211a4d745c3032fe2ad5040e227fa6d5fb1c83
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-21 14:34:37 +0530

    Initial Commit

---
+++ clone/src/main/java/com/upgrad/uber/clone/UberCloneApplication.java
@@ -0,0 +1,13 @@
+package com.upgrad.uber.clone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UberCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UberCloneApplication.class, args);
+	}
+
+}
---
+++ clone/src/test/java/com/upgrad/uber/clone/UberCloneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.upgrad.uber.clone;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UberCloneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
