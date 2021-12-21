5c4c885c207005c371cd44c17181a2de01174bac
faisalmoinuddin99 <faisal25march99@gmail.com>
2021-12-21 14:53:08 +0530

    FuelType Entity

---
+++ clone/src/main/java/com/upgrad/uber/clone/entities/FuelType.java
@@ -0,0 +1,68 @@
+package com.upgrad.uber.clone.entities;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+public class FuelType {
+
+    @Id
+    @Column(name = "fuel_type_id")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int fuelTypeId;
+
+    @Column(name="fuel_type",nullable = false,unique = true)
+    private String fuelType;
+
+
+    public FuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "fuelType",fetch = FetchType.LAZY,cascade = {CascadeType.ALL})
+    private Set<Vehicle> vehicles;
+
+    public FuelType() {
+
+    }
+
+    public int getFuelTypeId() {
+        return fuelTypeId;
+    }
+
+    public void setFuelTypeId(int fuelTypeId) {
+        this.fuelTypeId = fuelTypeId;
+    }
+
+    public String getFuelType() {
+        return fuelType;
+    }
+
+    public void setFuelType(String fuelType) {
+        this.fuelType = fuelType;
+    }
+
+    public Set<Vehicle> getVehicles() {
+        return vehicles;
+    }
+
+    public void setVehicles(Set<Vehicle> vehicles) {
+        this.vehicles = vehicles;
+    }
+
+    public FuelType(int fuelTypeId, String fuelType) {
+        this.fuelTypeId = fuelTypeId;
+        this.fuelType = fuelType;
+    }
+
+    @Override
+    public String toString() {
+        return "FuelType{" +
+                "fuelTypeId=" + fuelTypeId +
+                ", fuelType='" + fuelType + '\'' +
+                '}';
+    }
+}
